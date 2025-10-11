8df53e4c51bf78d498d9a971de02c7841c14c943
fooble2137 <github@fooble.dev>
2025-10-11 23:48:56 +0200

    Added initial files

---
+++ src/dev/fooble/snake/GameFrame.java
@@ -0,0 +1,6 @@
+package dev.fooble.snake;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+}
---
+++ src/dev/fooble/snake/GamePanel.java
@@ -0,0 +1,13 @@
+package dev.fooble.snake;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
